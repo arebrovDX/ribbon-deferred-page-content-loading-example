15dd63b7eaa929bd0ac2fde9e324d0ff3696b03c
Artem Rebrov <artemp.rebrov@devexpress.com>
2021-04-06 15:40:52 +0300

    add example content

---
+++ CS/UseRibbonDeferredPageLoading/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.UI;
+using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UseRibbonDeferredPageLoading
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand OpenCommand { get; set; }
+        public MainWindowViewModel()
+        {
+            OpenCommand = new DelegateCommand(Open);
+        }
+        void Open()
+        {
+            OpenFileDialogService openFileDialogService = new OpenFileDialogService();
+            if(openFileDialogService.ShowDialog())
+            { 
+                ThemedMessageBox.Show(openFileDialogService.GetFullFileName());
+            }
+        }
+    }
+}
